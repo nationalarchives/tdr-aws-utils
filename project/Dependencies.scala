8d3e0c42cc0444c2665a68c21984ca0f6df3eb4c
sam <sam.palmer@nationalarchives.gov.uk>
2020-10-06 13:05:22 +0100

    Add SES Client

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,6 +6,7 @@ object Dependencies {
   lazy val lambdaJavaCore = "com.amazonaws" % "aws-lambda-java-core" % "1.2.1"
   lazy val lambdaJavaEvents = "com.amazonaws" % "aws-lambda-java-events" % "3.1.0"
   lazy val s3Sdk = "software.amazon.awssdk" % "s3" % "2.13.18"
+  lazy val ses = "software.amazon.awssdk" % "ses" % "2.15.2"
   lazy val sqsSdk = "software.amazon.awssdk" % "sqs" % "2.13.15"
   lazy val typesafe = "com.typesafe" % "config" % "1.4.0"
   lazy val circeCore = "io.circe" %% "circe-core" % "0.13.0"
