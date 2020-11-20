3d9eaedcffca8e80e52407e9836cc9336e80efca
sam <sam.palmer@nationalarchives.gov.uk>
2020-11-20 10:38:48 +0000

    Add return type of method

--- src/main/scala/uk/gov/nationalarchives/aws/utils/S3Utils.scala
+++ src/main/scala/uk/gov/nationalarchives/aws/utils/S3Utils.scala
@@ -5,14 +5,14 @@ import java.nio.file.{Path, Paths}
 import cats.effect.IO
 import monix.catnap.syntax._
 import software.amazon.awssdk.services.s3.S3AsyncClient
-import software.amazon.awssdk.services.s3.model.{GetObjectRequest, PutObjectRequest, PutObjectResponse}
+import software.amazon.awssdk.services.s3.model.{GetObjectRequest, GetObjectResponse, PutObjectRequest, PutObjectResponse}
 
 class S3Utils(client: S3AsyncClient) {
   def upload(bucket: String, key: String, path: Path): IO[PutObjectResponse] = {
     IO(client.putObject(PutObjectRequest.builder.bucket(bucket).key(key).build, path)).futureLift
   }
 
-  def downloadFiles(bucket: String, key: String, path: Option[Path] = None) = {
+  def downloadFiles(bucket: String, key: String, path: Option[Path] = None): IO[GetObjectResponse] = {
     IO(client.getObject(GetObjectRequest.builder.bucket(bucket).key(key).build, path.getOrElse(Paths.get(key)))).futureLift
   }
 }
