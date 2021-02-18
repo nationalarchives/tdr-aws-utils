549791645433d9705f244461194f8ebd6a8dd8d1
TomJKing <tom.king@nationalarchives.gov.uk>
2021-02-18 11:16:43 +0000

    Set default error message

--- src/main/scala/uk/gov/nationalarchives/aws/utils/StepFunctionUtils.scala
+++ src/main/scala/uk/gov/nationalarchives/aws/utils/StepFunctionUtils.scala
@@ -4,7 +4,8 @@ import cats.effect.IO
 import io.circe.Json
 import monix.catnap.syntax.SyntaxForLiftFuture
 import software.amazon.awssdk.services.sfn.SfnAsyncClient
-import software.amazon.awssdk.services.sfn.model.{SendTaskSuccessRequest, SendTaskSuccessResponse}
+import software.amazon.awssdk.services.sfn.model.{SendTaskFailureRequest, SendTaskFailureResponse, SendTaskSuccessRequest, SendTaskSuccessResponse}
+import uk.gov.nationalarchives.aws.utils.StepFunctionUtils.defaultErrorMessage
 
 class StepFunctionUtils(client: SfnAsyncClient) {
 
@@ -16,8 +17,18 @@ class StepFunctionUtils(client: SfnAsyncClient) {
 
     IO(client.sendTaskSuccess(request)).futureLift
   }
+
+  def sendTaskFailureRequest(taskToken: String, error: Option[String]): IO[SendTaskFailureResponse] = {
+    val request = SendTaskFailureRequest.builder
+      .taskToken(taskToken)
+      .error(error.getOrElse(defaultErrorMessage))
+      .build
+
+    IO(client.sendTaskFailure(request)).futureLift
+  }
 }
 
 object StepFunctionUtils {
+  private val defaultErrorMessage = "Unknown task failure"
   def apply(client: SfnAsyncClient): StepFunctionUtils = new StepFunctionUtils(client)
 }
