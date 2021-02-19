21ea3455c67c97c824dd069c49d23c30e2666f76
TomJKing <tom.king@nationalarchives.gov.uk>
2021-02-19 08:15:40 +0000

    Add unit tests for new function

--- src/test/scala/uk/gov/nationalarchives/aws/utils/StepFunctionUtilsTest.scala
+++ src/test/scala/uk/gov/nationalarchives/aws/utils/StepFunctionUtilsTest.scala
@@ -9,13 +9,14 @@ import org.scalatest.EitherValues
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers._
 import software.amazon.awssdk.services.sfn.SfnAsyncClient
-import software.amazon.awssdk.services.sfn.model.{SendTaskSuccessRequest, SendTaskSuccessResponse}
+import software.amazon.awssdk.services.sfn.model.{SendTaskFailureRequest, SendTaskFailureResponse, SendTaskSuccessRequest, SendTaskSuccessResponse}
 import uk.gov.nationalarchives.aws.utils.TestUtils.failedFuture
 
 class StepFunctionUtilsTest extends AnyFlatSpec with MockitoSugar with EitherValues {
 
   private val taskToken: String = "taskToken"
   private val outputJson = Json.fromString("outputJson")
+  private val errorMessage = "some error occurred"
 
   "the sendTaskSuccessRequest method" should "be called with the correct parameters" in {
     val stepFunctionClient = Mockito.mock(classOf[SfnAsyncClient])
@@ -41,5 +42,31 @@ class StepFunctionUtilsTest extends AnyFlatSpec with MockitoSugar with EitherVal
     val stepFunctionUtils = StepFunctionUtils(stepFunctionClient)
     val response = stepFunctionUtils.sendTaskSuccessRequest(taskToken, outputJson).attempt.unsafeRunSync()
     response.left.value.getMessage should equal("Task success failed")
+  }
+
+  "the sendTaskFailureRequest method" should "be called with the correct parameters" in {
+    val stepFunctionClient = Mockito.mock(classOf[SfnAsyncClient])
+    val stepFunctionUtils = StepFunctionUtils(stepFunctionClient)
+
+    val argumentCaptor: ArgumentCaptor[SendTaskFailureRequest] = ArgumentCaptor.forClass(classOf[SendTaskFailureRequest])
+    val response = SendTaskFailureResponse.builder.build
+
+    when(stepFunctionClient.sendTaskFailure(argumentCaptor.capture())).thenReturn(CompletableFuture.completedFuture(response))
+
+    stepFunctionUtils.sendTaskFailureRequest(taskToken, Some(errorMessage)).unsafeRunSync()
+
+    val request: SendTaskFailureRequest = argumentCaptor.getValue
+    request.taskToken should equal("taskToken")
+    request.error should equal(errorMessage)
+  }
+
+  "The sendTaskFailureRequest method" should "return an error if the request fails" in {
+    val stepFunctionClient = Mockito.mock(classOf[SfnAsyncClient])
+    when(stepFunctionClient.sendTaskFailure(any[SendTaskFailureRequest]))
+      .thenReturn(failedFuture(new RuntimeException("Task failure request failed")))
+
+    val stepFunctionUtils = StepFunctionUtils(stepFunctionClient)
+    val response = stepFunctionUtils.sendTaskFailureRequest(taskToken, Some(errorMessage)).attempt.unsafeRunSync()
+    response.left.value.getMessage should equal("Task failure request failed")
   }
 }
