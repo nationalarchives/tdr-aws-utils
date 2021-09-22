2c0b001af732dd27da540e836098e5600ba7edad
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2021-09-22 10:58:05 +0100

    Remove whitespace changes

--- src/main/scala/uk/gov/nationalarchives/aws/utils/KMSUtils.scala
+++ src/main/scala/uk/gov/nationalarchives/aws/utils/KMSUtils.scala
@@ -22,18 +22,18 @@ class KMSUtils(client: KmsClient, encryptionContext: Map[String, String]) {
   }
 
   def decryptValue(value: String): String = {
-      Try {
-        val decodedValue = Base64.getDecoder.decode(value)
-        val decryptRequest = DecryptRequest.builder()
-          .ciphertextBlob(SdkBytes.fromByteBuffer(ByteBuffer.wrap(decodedValue)))
-          .encryptionContext(encryptionContext.asJava)
-          .build()
-        client.decrypt(decryptRequest).plaintext().asUtf8String()
-      } match {
-        // Return the original value on error. This will allow us to deploy without breaking the lambdas. It can be removed once all variables are encrypted
+    Try {
+      val decodedValue = Base64.getDecoder.decode(value)
+      val decryptRequest = DecryptRequest.builder()
+        .ciphertextBlob(SdkBytes.fromByteBuffer(ByteBuffer.wrap(decodedValue)))
+        .encryptionContext(encryptionContext.asJava)
+        .build()
+      client.decrypt(decryptRequest).plaintext().asUtf8String()
+    } match {
+      // Return the original value on error. This will allow us to deploy without breaking the lambdas. It can be removed once all variables are encrypted
       case Failure(_) => value
-        case Success(value) => value
-      }
+      case Success(value) => value
+    }
   }
 }
 
