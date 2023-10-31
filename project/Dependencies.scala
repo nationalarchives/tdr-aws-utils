47c1625cbb3636d54647c389cde6f70528ac9e27
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2023-10-31 08:24:46 +0000

    Update mockito-scala to 1.17.29

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   private val circeVersion = "0.14.6"
 
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.17"
-  lazy val mockito = "org.mockito" %% "mockito-scala" % "1.17.27"
+  lazy val mockito = "org.mockito" %% "mockito-scala" % "1.17.29"
   lazy val lambdaJavaCore = "com.amazonaws" % "aws-lambda-java-core" % "1.2.3"
   lazy val lambdaJavaEvents = "com.amazonaws" % "aws-lambda-java-events" % "3.11.3"
   lazy val s3Sdk = "software.amazon.awssdk" % "s3" % awsSdkVersion
