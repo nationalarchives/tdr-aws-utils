72e29cc6f959cb9987e1cb05b102d8035dbc2bac
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2023-11-28 08:24:15 +0000

    Update ecr, kms, s3, ses, sfn, sns, sqs to 2.21.32

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Dependencies {
-  private val awsSdkVersion = "2.21.29"
+  private val awsSdkVersion = "2.21.32"
   private val circeVersion = "0.14.6"
 
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.17"
