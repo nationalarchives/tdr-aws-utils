5ced7eb557e5714c90cbe9c6ae6945e0254dad60
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2024-06-25 08:23:12 +0000

    Update scalatest to 3.2.19 in scala-steward-dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
   private val awsSdkVersion = "2.26.7"
   private val circeVersion = "0.14.8"
 
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.18"
+  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.19"
   lazy val mockito = "org.mockito" %% "mockito-scala" % "1.17.31"
   lazy val lambdaJavaCore = "com.amazonaws" % "aws-lambda-java-core" % "1.2.3"
   lazy val lambdaJavaEvents = "com.amazonaws" % "aws-lambda-java-events" % "3.11.6"
