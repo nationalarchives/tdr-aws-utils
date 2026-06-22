a9c4f5e7f7304c7c74a7459f80a6d134ba6a0687
VimleshGupta <vimlesh.gupta@nationalarchives.gov.uk>
2026-06-22 11:59:14 +0100

    Update AWS SDK clients to use Apache5HttpClient

--- secretsmanager/src/main/scala/uk/gov/nationalarchives/aws/utils/secretsmanager/SecretsManagerClients.scala
+++ secretsmanager/src/main/scala/uk/gov/nationalarchives/aws/utils/secretsmanager/SecretsManagerClients.scala
@@ -1,6 +1,6 @@
 package uk.gov.nationalarchives.aws.utils.secretsmanager
 
-import software.amazon.awssdk.http.apache.ApacheHttpClient
+import software.amazon.awssdk.http.apache5.Apache5HttpClient
 import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient
 
@@ -9,7 +9,7 @@ import java.net.URI
 
 object SecretsManagerClients {
   def secretsmanager(endpoint: String): SecretsManagerClient = {
-    val httpClient = ApacheHttpClient.builder.build
+    val httpClient = Apache5HttpClient.builder.build
     SecretsManagerClient.builder
       .region(Region.EU_WEST_2)
       .endpointOverride(URI.create(endpoint))
